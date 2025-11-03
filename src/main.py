8ffe7b8d0e79a14804b6f57be2f75c188baaed49
FilipStawczyk <163164320+FilipStawczyk@users.noreply.github.com>
2025-11-03 15:24:40 +0000

    zadanie 2.1

--- src/main.py
+++ src/main.py
@@ -1,4 +1,13 @@
 #Filip Stawczyk
 
-print("Hello, Matura 2025!")
-#komentarz
+with open("/workspaces/matura-2025-r25-3a-FilipStawczyk/zalaczniki-2025/symbole.txt", "r") as plik:
+    linijki = [linia.strip() for linia in plik.readlines()]
+
+palindromy = []
+
+for linia in linijki:
+    if linia == linia[::-1]:
+        palindromy.append(linia)
+
+for p in palindromy:
+    print(p)
