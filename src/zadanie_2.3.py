ee897a6420df396a0d84c56ff85f5bf3a0edbe56
FilipStawczyk <163164320+FilipStawczyk@users.noreply.github.com>
2025-11-13 10:25:40 +0000

    dalsze zmiany w 2.3

--- src/zadanie_2.3.py
+++ src/zadanie_2.3.py
@@ -1,7 +1,10 @@
 # Filip Stawczyk
 
 def konwersja(liczba): #liczba jest w stringu
-    wynik = 177147 * int(liczba[0]) + 59049 * int(liczba[1])
+    wynik = 0
+    for i in range(12):
+        wynik += 3 ** (11-i) * liczba[i]
+    wynik = 177147 * int(liczba[0]) + 59049 * int(liczba[1]) + 19683 * int(liczba[2]) + 6561 * int(liczba[3]) + 2187*int(liczba[4]) + 729*int(liczba[5])
     return wynik
 
 with open("/workspaces/matura-2025-r25-3a-FilipStawczyk/zalaczniki-2025/symbole_przyklad.txt", "r") as plik:
